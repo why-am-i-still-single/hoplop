eac8320f636dba6aa64134df20bdfb64156f45f7
why-am-i-still-single <118999153+why-am-i-still-single@users.noreply.github.com>
2022-12-11 02:01:37 +0700

    Add files via upload
    create_hoplop

---
+++ gift.js
@@ -0,0 +1,46 @@
+
+var to = '12B8';
+var gift_url = 'https://www.facebook.com/ai.my.9041083/videos/411095377323906'
+var gift_image_url = 'tet.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+  document.getElementById('card').classList.add('cardshow')
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
